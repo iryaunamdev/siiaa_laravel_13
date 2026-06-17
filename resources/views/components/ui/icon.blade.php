159f0dea24a5ddd2270a4308fdf7ae77fa4c21ac
Leonardo <laonrdo@gmail.com>
2026-06-17 10:25:03 -0600

    Corrige namespace SVG en componente icon

--- resources/views/components/ui/icon.blade.php
+++ resources/views/components/ui/icon.blade.php
@@ -16,7 +16,7 @@
         @break
 
         @case('edit')
-            <svg {{ $attributes->merge(['class' => 'size-4']) }} viewBox="0 0 16 16" xmlns="http://www.w3.org/zinc0/svg"
+            <svg {{ $attributes->merge(['class' => 'size-4']) }} viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"
                 aria-hidden="true">
                 <path fill="currentColor"
                     d="M15.49 7.3h-1.16v6.35H1.67V3.28H8V2H1.67A1.21 1.21 0 0 0 .5 3.28v10.37a1.21 1.21 0 0 0 1.17 1.25h12.66a1.21 1.21 0 0 0 1.17-1.25z" />
@@ -35,7 +35,7 @@
         @break
 
         @case('plus')
-            <svg {{ $attributes->merge(['class' => 'size-5']) }} viewBox="0 0 20 20" xmlns="http://www.w3.org/zinc0/svg"
+            <svg {{ $attributes->merge(['class' => 'size-5']) }} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
                 aria-hidden="true">
                 <path fill="currentColor" fill-rule="evenodd"
                     d="M9 17a1 1 0 102 0v-6h6a1 1 0 100-2h-6V3a1 1 0 10-2 0v6H3a1 1 0 000 2h6v6z" clip-rule="evenodd" />
