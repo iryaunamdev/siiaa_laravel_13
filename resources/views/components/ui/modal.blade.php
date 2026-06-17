68a1463c8cbada92bdc1dfe94bed7ea2cef06d60
Leonardo <laonrdo@gmail.com>
2026-06-17 10:26:21 -0600

    Permite acción opcional al cerrar modal UI

--- resources/views/components/ui/modal.blade.php
+++ resources/views/components/ui/modal.blade.php
@@ -1,6 +1,7 @@
 @props([
     'maxWidth' => '2xl',
     'footerPosition' => 'right',
+    'closeAction' => null,
 ])
 
 @php
@@ -28,13 +29,14 @@
     };
 @endphp
 
-<div x-data="{ show: @entangle($attributes->wire('model')) }" x-show="show" x-cloak class="fixed inset-0 z-50 flex items-center justify-center px-4 py-6"
-    style="display: none;">
+<div x-data="{ show: @entangle($attributes->wire('model')) }" x-show="show" x-cloak
+    class="fixed inset-0 z-50 flex items-center justify-center px-4 py-6" style="display: none;">
     {{-- Overlay --}}
     <div x-show="show" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0"
         x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in duration-150"
         x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
-        class="absolute inset-0 bg-zinc-900/45 backdrop-blur-[2px]" x-on:click="show = false"></div>
+        class="absolute inset-0 bg-zinc-900/45 backdrop-blur-[2px]"
+        x-on:click="@if ($closeAction) $wire.{{ $closeAction }}() @else show = false @endif"></div>
 
     {{-- Contenedor del modal --}}
     <div x-show="show" x-transition:enter="transition ease-out duration-200"
